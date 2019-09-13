f645ba1793ab4eac8e9dec9b898cb1db45f26b9e
Marcus Heese <marcus.heese@gmail.com>
2019-09-13 13:23:44 -0700

    initial commit for MH45

---
+++ keyboards/mh45/mk1/rev0/keymaps/default/keymap.c
@@ -0,0 +1,34 @@
+/* Copyright 2019 Marcus Heese
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+#define _BASE 0
+#define _FN 1
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT( /* Base */
+        KC_ESC,    KC_Q,     KC_P,    KC_Y,    KC_C, KC_B,     KC_V,      KC_M,    KC_U,    KC_Z,     KC_L,   KC_MINUS,  KC_BSPACE,
+        KC_TAB,    KC_A,     KC_N,    KC_I,    KC_S, KC_F,     KC_D,      KC_T,    KC_H,    KC_O,     KC_R,   KC_QUOTE,  KC_ENTER,
+        KC_LSHIFT, KC_COMMA, KC_DOT,  KC_J,    KC_G, KC_SLASH, KC_SCOLON, KC_W,    KC_K,    KC_X,     KC_UP,  KC_RSHIFT,
+        MO(_FN),   KC_LCTRL, KC_LALT, KC_LGUI, KC_E, KC_SPACE, KC_RGUI,   KC_LEFT, KC_DOWN, KC_RIGHT, MO(_FN)
+    ),
+    [_FN] = LAYOUT(
+        KC_GRAVE,       LSFT(KC_1),     LSFT(KC_2),     LSFT(KC_3),     LSFT(KC_4),  LSFT(KC_5),     LSFT(KC_6),     LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0), LSFT(KC_EQUAL), KC_DELETE,
+        KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,        KC_5,           KC_6,           KC_7,       KC_8,       KC_9        KC_0,       KC_EQUAL,       KC_TRANSPARENT,
+        KC_TRANSPARENT, KC_NO,          KC_NO,          KC_NO,          KC_LBRACKET, KC_BSLASH,      KC_RBRACKET,    KC_NO,      KC_NO,      KC_NO,      KC_PGUP,    KC_TRANSPARENT,
+        KC_NO,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_NO,       KC_TRANSPARENT, KC_TRANSPARENT, KC_HOME,    KC_PGDOWN,  KC_END,     KC_NO
+    )
+};
---
+++ keyboards/mh45/mk1/rev0/readme.md
@@ -0,0 +1,19 @@
+# MH45 Mk I (Rev0)
+
+![MH45 Mk I (Rev0)](http://www.keyboard-layout-editor.com/#/gists/f9c47230b040e785f7b0bdc2dfc172d1)
+
+This is an idea for a ~45% staggered layout featuring the MALTRON letter layout.
+It is an attempt to build a portable keyboard which still features the MALTRON layout *and* has a set of arrow keys.
+With 13.25u it is as small as it can get for a staggered layout for MALTRON.
+Going for 40% is only possible in an ortholinear layout as 40% staggered layouts (that fit planck cases) only keep 3 keys on the home row for the right hand.
+However, this is impossible for the MALTRON layout.
+
+Keyboard Maintainer: [Marcus Heese](https://github.com/mheese)
+Hardware Supported: Rev0 is handwired, using an Elite-C controller
+Hardware Availability: DIY
+
+Make example for this keyboard (after setting up your build environment):
+
+    make mh45/mk1/rev0:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
