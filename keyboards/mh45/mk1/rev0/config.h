e053c0fd30fb4c0650a832668c953ef5f9b4af98
Marcus Heese <marcus.heese@gmail.com>
2019-09-26 22:07:55 -0700

    forgot to commit

--- keyboards/mh45/mk1/rev0/config.h
+++ keyboards/mh45/mk1/rev0/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID 0x0001
 #define DEVICE_VER 0x0001
 #define MANUFACTURER mheese
-#define PRODUCT "MH45 Mk I (Rev0)"
+#define PRODUCT MH45 Mk I [Rev0]
 #define DESCRIPTION "A 45pct staggered keyboard featuring the MALTRON letter layout"
 
 /* key matrix size */
